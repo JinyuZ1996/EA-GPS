3d3300d169c61bd54c06f9f3537daa1b2a18b555
JinyuZ1996 <jinyuz1996@outlook.com>
2025-02-25 19:48:31 +0800

    Delete Data_Generator directory

--- Data_Generator/Generate_Dict.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-# df = pd.read_csv("../Data_Generator/Ulist.txt", sep='\t', header=None)
-# df.to_csv('../Data_Generator/User_Dict.csv', index=False, sep=',', encoding='utf-8', header=None)
-
-df = pd.read_csv("../Data_Generator/Movie_Dict.csv", sep=',', header=None)
-df.to_csv('../Data_Generator/Movie_Dict.txt', index=False, sep='\t', encoding='utf-8', header=None)
